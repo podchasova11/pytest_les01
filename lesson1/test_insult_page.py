83e8ba8b4f20fc33a874ffa952142fe07adb3910
Mila Podchasova <Pod4asova20@gmail.com>
2023-10-04 21:11:50 +0300

    Update test_insult_page.py

--- lesson1/test_insult_page.py
+++ lesson1/test_insult_page.py
@@ -5,4 +5,4 @@ class TestInsultPage:
 
     def test_generate_insult(self):
         response = requests.get("https://evilinsult.com/generate_insult.php")
-        assert response.status_code == 200, "Запрос на получение оскорбления сломан"
+        assert response.status_code == 200, "Запрос сломан"
