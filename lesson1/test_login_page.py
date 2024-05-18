af33cea2e691d45a4b4fd5d9c758e9f412e3e4b5
Mila Podchasova <Pod4asova20@gmail.com>
2024-05-18 03:14:27 +0300

    Update test_login_page.py

--- lesson1/test_login_page.py
+++ lesson1/test_login_page.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 class TestLoginPage: # Название тестового класса
 
     def test_open_login_page(self):
